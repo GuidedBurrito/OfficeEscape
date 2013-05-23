50da50c37fe24a3e83748e3a8e78ca3d95af5e42
Alex Valickis <alexvalickis@hotmail.com>
2013-05-23 13:00:16 -0400

    Lots of Bugs Fixed
    Most of bug list completed
    some different sleep timers added

--- main.py
+++ main.py
@@ -80,7 +80,8 @@ def chooseSpotOption():
     print ('As you scope out the office you see the boss is making his rounds.')
     print ('He is a couple cubicles away from reaching yours. And then it hits you....')
     time.sleep(5)
-    print ('Youve shut your computer down already. If he sees your not doing anything your busted!')
+    print ('You have fully shutdown your computer already.')
+    print ('If he sees your not doing anything your busted!')
     SpotOption = ''
     while SpotOption != 'Start' and SpotOption != 'START' and SpotOption != 'start' and SpotOption != 'Fax' and SpotOption != 'fax' and SpotOption != 'FAX':
         print ('Do you try and START your computer up again or')
@@ -123,9 +124,9 @@ def chooseLieOption():
     else :
         print
         print
-        print ('You sheepishly tell him no and appologise in hopes that he forgets all of this and moves on.')
-        print ('After some short grovilling, he accepts your appology but will now be keeping a close eye on you.')
-        print ('There will be no more early weekend for you in the future.')
+        print ('You sheepishly tell him no and appologise in hopes that he forgets all of this')
+        print ('and moves on. After some short grovilling, he accepts your appology but will')
+        print ('be keeping a close eye on you. There will be no early weekends in your future.')
         print
         win = False
     return win 
@@ -142,28 +143,32 @@ def chooseStretchOption(Name):
     print ('He asks "While your up ' + Name + ', can you get me a coffee?"')
     StretchOption = ''
     while StretchOption != 'Coffee' and StretchOption != 'COFFEE' and StretchOption != 'coffee' and StretchOption != 'Early' and StretchOption != 'EARLY' and StretchOption != 'early': 
-        print ('Do you go get the boss a COFFEE or say youll go and start your weekend EARLY? (Coffee or Early)')
+        print ('Do you go get the boss a COFFEE or ditch and start your weekend EARLY?')
+        print ('(Coffee or Early)')
         print
         StretchOption = raw_input()
     if StretchOption == 'Coffee' or StretchOption == 'COFFEE' or StretchOption == 'coffee':
         print
         print
-        print ('You happily agree to getting coffe not only because you didnt get fired but you also')
-        print ('get to leave the office. Unfortunatly between getting to the coffee shop, waiting in line,')
-        print ('and travelling back you spend most of the day getting coffee and end up leaving the same')
-        print ('time that you alway do')
+        print ('You happily agree to getting coffe not only because you didnt get')
+        print ('fired but you also get to leave the office. Unfortunatly between')
+        print ('getting to the coffee shop, waiting in line, and travelling back')
+        print ('you spend most of the day getting coffee and end up leaving the')
+        print ('same time that you alway do.')
         print
         win = False
     else :
         print
         print
-        print ('You happily agree to getting your boss coffe only because your not coming back!')
-        print ('You got to leave early and meet up with your friends. The weekend was almost perfect...')
-        time.sleep(1)
+        print ('You happily agree to getting your boss coffee only because your not')
+        print ('coming back! You got to leave early and meet up with your friends.')
+        print ('The weekend was almost perfect...')
+        time.sleep(5)
         print ('Untill you found a message left by your boss on your answering machine at home.')
-        print ('He was waiting for you to come back, it was only until later that weekend when he saw')
-        print ('the photos of you partying with your friends that he knew you skipped getting his coffee.')
-        print ('At the creshedo of his rant he tells you not to bother coming in to work on Teusday,')
+        print ('He was waiting for you to come back, it was only until later that weekend when')
+        print ('he saw the photos of you partying with your friends that he knew you skipped')
+        print (' getting his coffee. At the creshedo of his rant he tells you not to bother')
+        print ('coming in to work on Teusday,')
         print
         print ("You've Been Fired")
         print
@@ -177,21 +182,26 @@ def chooseStartOption(Name):
     print ('The Boot-Up')
     print
     print
-    print ('You quickly turn your computer back on hoping that it will get up and runnig quickly.')
+    print ('You quickly turn your computer back on hoping that it will ready.')
     print ('Unfortunatly for you, the computer runs as if hamsters are powering it.')
-    print ('As you pound at your keyboard in an atempt to get it to start faster, the boss comes in.')
+    print ('As you pound at your keyboard trying to get it to start faster...')
+    time.sleep(2)
+    print ('The boss comes in.')
     print ('"Whats going on with you computer ' + Name + '?')
     StartOption = ''
     while StartOption != 'Crash' and StartOption != 'crash' and StartOption != 'CRASH' and StartOption != 'Update' and StartOption != 'update' and StartOption != 'UPDATE':
-        print ('Do you tell your boss it was a compute CRASH or your preforming an UPDATE? (Crash or Update')
+        print ('Do you tell your boss it was a computer CRASH or your preforming an UPDATE?')
+        print ('(Crash or Update)')
         print
         StartOption = raw_input()
     if StartOption == 'Crash' or StartOption == 'crash' or StartOption == 'CRASH':
         print
         print
-        print ('You give your boss the best and most convincing lie that you have ever come up with....')
-        time.sleep(1)
-        print ('And he baught it! "Well thats really unfortunate ' + Name + ', you must have lost everything!')
+        print ('You give the boss your most convincing lie that you have ever come up with...')
+        time.sleep(2)
+        print ('And he baught it!')
+        time.sleep(2)
+        print ('"Well thats really unfortunate ' + Name + ', you must have lost everything!')
         print ('Uh-oh, you know where this is going but have no idea how to stop it')
         print ('"I guess youll need to stay late to catch up eith all that work that you lost"')
         print
@@ -199,10 +209,10 @@ def chooseStartOption(Name):
     else :
         print
         print
-        print ('You swallow your overwelming nervousness and tell your boss "Its just a routine update.')
-        print ('"An update you say" says the boss, "Well thats great! I want you to install this update')
-        print ('on everyones system!". Knowing that you have to maintain the lie or else you get fired,')
-        print ('you spend the rest of the day pretending to install your imaginary update.')
+        print ('Trying not to panic you tell your boss "Its just a routine update"')
+        print ('"An update?" says the boss, "Thats great! I want you to install this update')
+        print ('on everyones system!". Knowing that you have to maintain the lie or youll get')
+        print ('fired, you spend the rest of the day pretending to install your imaginary update')
         print
         win = False
     return win
@@ -215,11 +225,13 @@ def chooseFaxOption():
     print
     print
     print ('Yor franticly search your desk and grab some loose papers.')
-    print ('Before you even have time to straighten out the papers the boss peeks inside your cubicle')
+    print ('Before you even have time to straighten out the papers the')
+    print ('boss peeks inside your cubicle')
     FaxOption = ''
     while FaxOption != 'Send' and FaxOption != 'send' and FaxOption != 'SEND' and FaxOption != 'Hope' and FaxOption != 'hope' and FaxOption != 'HOPE':  
-        print ('He isnt leaving, do you quickly throw the papers into the fax machine and SEND it to a made up number')
-        print ('or do you organize the papers and HOPE he leaves. (Send or Hope)')
+        print ('He isnt leaving, do you quickly throw the papers into the fax machine')
+        print ('and SEND it to a made up number or do you organize the papers and HOPE he leaves.')
+        print ('(Send or Hope)')
         print
         FaxOption = raw_input()
     if FaxOption == 'Send' or FaxOption == 'SEND' or FaxOption == 'send':
